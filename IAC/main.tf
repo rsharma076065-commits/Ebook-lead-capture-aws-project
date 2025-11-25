11778d0b8aac3b0076a4f13d9803ef09615c7f39
Rishav Sharma <rsharma076065-commits>
2025-11-25 17:01:11 +0000

    my second commit

--- IAC/main.tf
+++ IAC/main.tf
@@ -65,3 +65,101 @@ resource "null_resource" "sync_s3_folder" {
   ]
 }
 
+
+data "aws_route53_zone" "main" {
+  name         = "rishavops.online"
+  private_zone = false
+}
+
+resource "aws_acm_certificate" "rishavops_certificate" {
+  domain_name       = "rishavops.online"
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "rishavops_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.rishavops_certificate.domain_validation_options :
+    dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  zone_id         = data.aws_route53_zone.main.id
+  name            = each.value.name
+  type            = each.value.type
+  records         = [each.value.record]
+  ttl             = 60
+  allow_overwrite = true
+}
+
+resource "aws_acm_certificate_validation" "rishavops_certificate_validation" {
+  certificate_arn         = aws_acm_certificate.rishavops_certificate.arn
+  validation_record_fqdns = [for rec in aws_route53_record.rishavops_validation : rec.fqdn]
+
+  timeouts {
+    create = "60m"
+  }
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.my_bucket.bucket_domain_name
+    origin_id   = "S3-my-bucket-origin"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "CDN for S3 static website"
+  default_root_object = "index.html"
+
+  aliases = ["rishavops.online"]
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3-my-bucket-origin"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  viewer_certificate {
+    acm_certificate_arn      = aws_acm_certificate.rishavops_certificate.arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2019"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  tags = {
+    Name = "S3StaticWebsiteCDN"
+  }
+}
+
+
